c5d2f8894f8b7f1e764846dcfa4539da539fe9a6
mad titan <thanos@nym.hush.com>
2020-06-05 20:07:42 +0300

    another test before I start from with another test branch

--- hello-cloudbuild/main_test.go
+++ hello-cloudbuild/main_test.go
@@ -17,8 +17,9 @@ func TestHandler(t *testing.T) {
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v, want %v", status, http.StatusOK)
 	}
-	expected := "Hello, 世界\n"
+	expected :=  "Geia1, 世界"
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v, want %v", rr.Body.String(), expected)
 	}
 }
+
